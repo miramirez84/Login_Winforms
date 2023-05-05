5bf0ed96808d33188b46777b64076112e053d128
PC-MARM-23\Miguel <ramirez.miguel@hotmail.es>
2023-05-05 00:06:30 -0400

    Se agrego la Clase Utils, que contiene metodos varios Se agrego el metodo Utils.GetServerDate()

---
+++ Login_Winforms/Manager/Utils.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Winforms.Manager
+{
+	internal class Utils
+	{
+		public static DateTime GetServerDate()
+		{
+			using (SqlConnection conn = new(Properties.Settings.Default.ConnectionString))
+			{
+				SqlCommand cmd = conn.CreateCommand();
+				cmd.CommandType = System.Data.CommandType.Text;
+				cmd.CommandText = "Select GetDate();";
+				
+				conn.Open();
+				DateTime ServerDate = (DateTime)cmd.ExecuteScalar();
+				return ServerDate;
+			}				
+		}
+	}
+}
